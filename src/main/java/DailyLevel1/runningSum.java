8f94545a75792bbdeb1c50bccc046224b3696f96
danieljames96 <danieljames96@gmail.com>
2022-10-15 15:25:27 +0530

    RunningSum completed

--- src/main/java/DailyLevel1/runningSum.java
+++ src/main/java/DailyLevel1/runningSum.java
@@ -1,4 +1,33 @@
 package DailyLevel1;
 
+import java.util.Arrays;
+
 public class runningSum {
+
+    public static int sum(int[] nums, int i){
+
+        int sum;
+
+        if(i >= 0){
+            sum=nums[i];
+            return sum+=sum(nums,i-1);
+        }
+        else{
+            return 0;
+        }
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int numsize = nums.length;
+        int[] runningsum = new int[numsize];
+        for (int i = 0; i < numsize; i++) {
+            runningsum[i] = sum(nums,i);
+        }
+        return runningsum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
 }
