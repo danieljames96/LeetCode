2c8a2f67f89874210adb225f7539e68517370785
danieljames96 <danieljames96@gmail.com>
2022-10-19 22:06:12 +0530

    maxProfit completed

---
+++ src/main/java/DailyLevel1/Greedy/maxProfit.java
@@ -0,0 +1,38 @@
+package DailyLevel1.Greedy;
+
+public class maxProfit {
+    public static int maxProfit(int[] prices) {
+        int length = prices.length;
+        int lsf = Integer.MAX_VALUE;
+        int op = 0, tp = 0;
+
+        if(length == 0 || length == 1){
+            return 0;
+        }
+        else if (length == 2){
+            if(prices[0] > prices[1]){
+                return 0;
+            }
+            else{
+                return prices[1] - prices[0];
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+            tp = prices[i] - lsf;
+            if(tp > op){
+                op = tp;
+            }
+        }
+
+        return op;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+}
