dadf43e2ac7cf93a1a9d81183d69def898f64ca3
Daniel James <34281186+danieljames96@users.noreply.github.com>
2025-04-27 19:54:35 +0530

    Time: 2480 ms (5.08%), Space: 30.7 MB (26.16%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,19 +1,28 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         
+        num_hash = {}
         removed = []
         count = 0
 
-        for i in range(len(nums)-1):
-            if i in removed:
-                continue
-            for j in range(i+1, len(nums)):
-                if j in removed:
-                    continue
-
-                if nums[i] + nums[j] == k:
-                    count += 1
-                    removed.extend([i, j])
-                    break
+        for i in range(len(nums)):
+            if num_hash.get(nums[i], 0) == 0:
+                num_hash[nums[i]] = 1
+            else:
+                num_hash[nums[i]] += 1
         
+        # print(f'num_hash: {num_hash}')
+
+        for key, value in num_hash.items():
+            if key in removed:
+                continue
+            
+            if k-key == key:
+                count += value//2
+            elif num_hash.get(k-key, 0) == 0:
+                continue
+            else:
+                count += min(value, num_hash.get(k-key, 0))
+                removed.append(k-key)
+
         return count
